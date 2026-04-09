53b9bc7f3b368c3828cec1cd4357b53de622eeea
VSUBASHRAJ <VADIVELR55416@GMAIL.COM>
2026-04-09 16:51:32 +0530

    added feature

--- flask-devops-app/scripts/send_report.py
+++ flask-devops-app/scripts/send_report.py
@@ -1,5 +1,14 @@
 import datetime
-report_file = "./logs/daily_report.txt"
+import os
+
+log_dir = "./logs"
+
+# Create folder if not exists
+os.makedirs(log_dir, exist_ok=True)
+
+report_file = os.path.join(log_dir, "daily_report.txt")
+
 with open(report_file, "a") as f:
     f.write(f"Report generated at {datetime.datetime.now()}\n")
+
 print(f"Report saved to {report_file}")
